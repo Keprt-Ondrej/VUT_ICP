6c41845920fc196e0e0bf533e67fb046b65d256f
Mliron <62309492+Mliron@users.noreply.github.com>
2021-05-07 18:17:53 +0200

    Updated main due to MQTT changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <fstream>
 #include <vector>
 #include <unistd.h>
-#include <memory> // unique_ptr
 #include "mqtt_client.h"
 #include <thread>
 #include "shared.h"
@@ -28,14 +27,26 @@ void thread_test(MQTT_Client& client){
 Shared *activeShared;
 int main(int argc, char *argv[]) {
   
+	/// Set up client info
+	MQTT_Client mqtt;
+	client_t data = {
+		"127.0.0.1", // only IP, DNS is not set up yet
+		6969,
+		"",
+		"",
+		""
+	};
+
+	/// Connect to MQTT broker
 	std::cout << "\nExpecting CONNACK(2)\n";
-	MQTT_Client mqtt("127.0.0.1", 6969);
+	mqtt.broker_connect(data);
 	if(mqtt.mqtt_recv(5)){
 		std::cerr << "Connection failed.\n";
 		return -1;
 	}
 	else std::cout << "Connection established.\n";
 
+	/// Start receiving packets
 	std::thread recv_thrd(thread_test, std::ref(mqtt));
 
 
@@ -43,28 +54,29 @@ int main(int argc, char *argv[]) {
 	int retval = mqtt.ping();
 	if(retval) std::cerr << "Function ping returned with: "<< retval << std::endl;
 	
-	std::cout << "Expecting None(PUBLISH with 0 QoS)\n";
-	retval = mqtt.publish("lol", "69");
+	std::cout << "Expecting PUBACK(PUBLISH with 1 QoS)\n";
+	retval = mqtt.publish("lol", "69", {0,1,1});
 	if(retval) std::cerr << "Function publish returned with: "<< retval << std::endl;
 	
 
 	std::cout << "Expecting SUBACK(9)&PUBLISH(3) because of retained value\n";
 	retval = mqtt.subscribe("#");
 	if(retval) std::cerr << "Function subscribe returned with: "<< retval << std::endl;
+	//retval = mqtt.subscribe("$SYS/#");
+	//if(retval) std::cerr << "Function subscribe returned with: "<< retval << std::endl;
 	
 	std::cout << "Expecting UNSUBACK(11)\n";
 	retval = mqtt.unsubscribe("lol");
 	if(retval) std::cerr << "Function unsubscribe returned with: "<< retval << std::endl;
 
-	std::cout << "Not expecting anything; just in case tere is one more\n";
-
-
-	std::string lol;
-	std::cin >> lol;
+	/// Leave it running until user closes it
+	//std::string lol;
+	//std::cin >> lol;
 
 	usleep(500000);
 	mqtt.broker_disconnect();
 	recv_thrd.join();
+	std::cout << "Error of -5 is ok, it means that broker is not available anymore\n";
 
   /* level00
 	 *  | topic0
