910a99dddc34bcc3ee4d6aa601d594b07acf06b4
Ondrej Keprt <keprt.ondrej@gmail.com>
2021-05-09 01:39:51 +0200

    dox setup

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,18 @@
 #include <iostream>
 #include <ctime>
 
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
 
 void thread_test(MQTT_Client& client){
 	int retval;
